791a6325b9b7a6196d33595208c10709c0a17a18
KRISHNA KUMAR GUPTA <61408269+krishnaa6268@users.noreply.github.com>
2022-12-29 18:54:53 +0530

    Add files via upload
    Base Project Files Uploaded

---
+++ PhoneBook Software/1-Phonebook (Main)/backup.sql
@@ -0,0 +1,7 @@
+insert into notebook.table2 values ('shiv' , 'Gwwalior' , '83187484');
+insert into notebook.table2 values ('rachitt' , 'Gwwalior' , '831874844');
+insert into notebook.table2 values ('rachit' , 'Gwwalior' , '8318748544');
+insert into notebook.table2 values ('krishnna' , 'gwalior' , '8318748559');
+insert into notebook.table2 values ('KRISHNA' , 'KALPI' , '880872085');
+insert into notebook.table2 values ('KRISHNA' , 'KALPI' , '8808720855');
+insert into notebook.table2 values ('SHIVANSH' , 'JALESAR' , '8845720855');
